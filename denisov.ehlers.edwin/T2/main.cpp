2ce9c7f9b6eca38bb957f0cae2bb54530e2f118b
Edwin Denisov-Elers <edwindenisovelers9864@gmail.com>
2025-04-02 21:30:31 +0300

    Comparator + failbits for istream

--- denisov.ehlers.edwin/T2/main.cpp
+++ denisov.ehlers.edwin/T2/main.cpp
@@ -1,26 +1,8 @@
+#include <iomanip>
 #include <iostream>
 #include <iterator>
 #include <vector>
 #include <regex>
-/*
- * 8. DBL SCI, SLL LIT
- *
- * [DBL SCI] Вещественное поле с двойной точностью (double) в научном
- * формате:
- * :keyX 5.45e-2:
- * :keyX 5.45E-2:
- * Число должно выводиться в стандартном виде, т.е. мантисса должна быть
- * меньше 10 и не меньше 1
- *
- * [SLL LIT] Знаковое максимально доступной ёмкости (long long) в формате
- * литерала:
- * :keyX 89ll:
- * :keyX -89LL:
- *
-*/
-
-// (:key1 5.45e-2:key2 89ll:key3 "Data":)
-// (:key2 89ll:key1 5.45e-2:key3 "Data":)
 
 struct DataStruct
 {
@@ -33,7 +15,10 @@ std::istream& operator>>(std::istream& in, DataStruct& data)
 {
   std::string line;
   if (!std::getline(in, line))
+  {
+    in.setstate(std::ios::failbit);
     return in;
+  }
 
   std::smatch match;
   const std::regex lineRegex(R"(\(:([^:]+:){3}\))");
@@ -43,27 +28,28 @@ std::istream& operator>>(std::istream& in, DataStruct& data)
 
   if (!std::regex_search(line, match, lineRegex))
   {
+    in.setstate(std::ios::failbit);
     return in;
   }
   if (!std::regex_search(line, match, key1Regex))
   {
+    in.setstate(std::ios::failbit);
     return in;
   }
-  std::cout << match[1].str() << '\n';
   data.key1 = std::stod(match[1].str());
 
   if (!std::regex_search(line, match, key2Regex))
   {
+    in.setstate(std::ios::failbit);
     return in;
   }
-  std::cout << match[1].str() << '\n';
   data.key2 = std::stoll(match[1].str());
 
   if (!std::regex_search(line, match, key3Regex))
   {
+    in.setstate(std::ios::failbit);
     return in;
   }
-  std::cout << match[1].str() << '\n';
   data.key3 = match[1].str();
 
   return in;
@@ -71,27 +57,36 @@ std::istream& operator>>(std::istream& in, DataStruct& data)
 
 std::ostream& operator<<(std::ostream& out, const DataStruct& data)
 {
-  out << "(:key1 " << std::scientific << data.key1
+  out << "(:key1 " << std::scientific << std::setprecision(1) << data.key1
     << ":key2 " << data.key2 << "ll"
     << ":key3 \"" << data.key3 << "\":)";
+
   return out;
 }
 
 int main()
 {
-  std::vector<DataStruct> data;
-  DataStruct ds;
-  std::cin >> ds;
-  std::cout << ds;
+  std::vector<DataStruct> dataVector;
 
-  // std::ostream_iterator<DataStruct> out(std::cout, ", ");
-  // std::copy(
-  //   std::istream_iterator<DataStruct>(std::cin),
-  //   std::istream_iterator<DataStruct>(),
-  //   std::back_inserter(data)
-  // );
-  //
-  // std::copy(data.begin(), data.end(), out);
+  const std::istream_iterator<DataStruct> begin(std::cin), end;
+  std::copy(begin, end, std::back_inserter(dataVector));
+
+  std::sort(dataVector.begin(), dataVector.end(),
+            [](const DataStruct& a, const DataStruct& b)
+            {
+              if (a.key1 != b.key1)
+              {
+                return a.key1 < b.key1;
+              }
+              if (a.key2 != b.key2)
+              {
+                return a.key2 < b.key2;
+              }
+              return a.key3.length() < b.key3.length();
+            });
+
+  const std::ostream_iterator<DataStruct> out(std::cout, "\n");
+  std::copy(dataVector.begin(), dataVector.end(), out);
 
   return 0;
 }

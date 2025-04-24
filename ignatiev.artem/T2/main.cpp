e43b134bc52fe4500dd3959d1b72d7a244c1420c
Ignatiev Artem <ignatiev.ak@edu.spbstu.ru>
2025-04-24 16:06:33 +0300

    fix

--- ignatiev.artem/T2/main.cpp
+++ ignatiev.artem/T2/main.cpp
@@ -7,13 +7,17 @@
 int main() {
     std::vector<DataStruct> items;
     DataStruct ds;
-    while (std::cin >> ds) {
-        items.push_back(ds);
-        if (std::cin.fail()) {
+    
+    while (!std::cin.eof()) {
+        std::cin >> ds;
+        if (std::cin) {
+            items.push_back(ds);
+        } else if (!std::cin.eof()) {
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
     }
+    
     sortData(items.begin(), items.end());
     std::ostream_iterator<DataStruct> out_it(std::cout, "\n");
     std::copy(items.begin(), items.end(), out_it);

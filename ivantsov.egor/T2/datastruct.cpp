03ac871cc64f8094990c64756577a3fa03a7d712
Иванцов Егор <iegorivantsovs@gmail.com>
2025-04-28 22:09:37 +0300

    third commit for T2

--- ivantsov.egor/T2/datastruct.cpp
+++ ivantsov.egor/T2/datastruct.cpp
@@ -77,6 +77,21 @@ static void trim(std::string& s)
   }
 }
 
+static size_t findNextColonOutsideQuotes(const std::string& str, size_t from)
+{
+  bool inQuotes = false;
+  for (size_t i = from; i < str.size(); ++i) {
+    char ch = str[i];
+    if (ch == '"') {
+      inQuotes = !inQuotes;
+    }
+    else if (ch == ':' && !inQuotes) {
+      return i;
+    }
+  }
+  return std::string::npos;
+}
+
 static bool parseLine(const std::string& original, DataStruct_t& outData)
 {
   std::string line = original;
@@ -98,11 +113,17 @@ static bool parseLine(const std::string& original, DataStruct_t& outData)
 
   while (true) {
     const size_t startColon = inner.find(':', pos);
-    if (startColon == std::string::npos) break;
+    if (startColon == std::string::npos) {
+      break;
+    }
     const size_t spacePos = inner.find(' ', startColon + 1);
-    if (spacePos == std::string::npos) break;
-    const size_t endColon = inner.find(':', spacePos + 1);
-    if (endColon == std::string::npos) break;
+    if (spacePos == std::string::npos) {
+      break;
+    }
+    const size_t endColon = findNextColonOutsideQuotes(inner, spacePos + 1);
+    if (endColon == std::string::npos) {
+      break;
+    }
 
     std::string fieldName = inner.substr(startColon + 1, spacePos - (startColon + 1));
     trim(fieldName);
@@ -114,18 +135,24 @@ static bool parseLine(const std::string& original, DataStruct_t& outData)
 
     if (fieldName == "key1") {
       double val = 0.0;
-      if (!parseDoubleLit(fieldValue, val)) return false;
+      if (!parseDoubleLit(fieldValue, val)) {
+        return false;
+      }
       outData.key1_ = val;
       hasKey1 = true;
     }
     else if (fieldName == "key2") {
       unsigned long long val = 0ULL;
-      if (!parseUllLit(fieldValue, val)) return false;
+      if (!parseUllLit(fieldValue, val)) {
+        return false;
+      }
       outData.key2_ = val;
       hasKey2 = true;
     }
     else if (fieldName == "key3") {
-      if (fieldValue.size() < 2 || fieldValue.front() != '"' || fieldValue.back() != '"') {
+      if (fieldValue.size() < 2 ||
+        fieldValue.front() != '"' ||
+        fieldValue.back() != '"') {
         return false;
       }
       outData.key3_ = fieldValue.substr(1, fieldValue.size() - 2);
@@ -163,7 +190,11 @@ std::ostream& operator<<(std::ostream& out, const DataStruct_t& data)
 
 bool dataStructComparator(const DataStruct_t& lhs, const DataStruct_t& rhs)
 {
-  if (lhs.key1_ != rhs.key1_) return lhs.key1_ < rhs.key1_;
-  if (lhs.key2_ != rhs.key2_) return lhs.key2_ < rhs.key2_;
+  if (lhs.key1_ != rhs.key1_) {
+    return lhs.key1_ < rhs.key1_;
+  }
+  if (lhs.key2_ != rhs.key2_) {
+    return lhs.key2_ < rhs.key2_;
+  }
   return lhs.key3_.size() < rhs.key3_.size();
 }

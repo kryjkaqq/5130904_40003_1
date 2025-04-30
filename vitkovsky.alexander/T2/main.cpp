b9a0a70912245e899d11e7adfd11a6bb64e99da3
Alexander <alsalindr@gmail.com>
2025-04-30 18:51:55 +0300

    All files for T2

--- vitkovsky.alexander/T2/main.cpp
+++ vitkovsky.alexander/T2/main.cpp
@@ -1,2 +1,33 @@
-int main()
-{}
+#include "DataStruct.h"
+#include <algorithm>
+#include <vector>
+#include <iterator>
+#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    using mynsp::DataStruct;
+
+    std::vector<DataStruct> data;
+
+    while (!std::cin.eof()) {
+        DataStruct temp;
+        if (std::cin >> temp) {
+            data.push_back(temp);
+        } else {
+            std::cin.clear(); // сбрасываем failbit, хотим продолжать
+            std::string skip;
+            std::getline(std::cin, skip);
+        }
+    }
+
+    std::sort(data.begin(), data.end(), mynsp::compareDataStruct);
+
+    std::copy(
+        data.begin(),
+        data.end(),
+        std::ostream_iterator<DataStruct>(std::cout, "\n")
+    );
+
+    return 0;
+}
